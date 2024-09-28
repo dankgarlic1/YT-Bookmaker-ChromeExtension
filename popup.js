8c9347f61e81507ce38c3607003132beea7002ea
Hashit <raizadaharshit2004@gmail.com>
2024-09-28 11:07:12 +0530

    ok

--- popup.js
+++ popup.js
@@ -1,12 +1,26 @@
+import { getActiveTabURL } from "./utils";
+
 // adding a new bookmark row to the popup
 const addNewBookmark = () => {};
 
 const viewBookmarks = () => {};
 
-const onPlay = e => {};
+const onPlay = (e) => {};
 
-const onDelete = e => {};
+const onDelete = (e) => {};
 
-const setBookmarkAttributes =  () => {};
+const setBookmarkAttributes = () => {};
 
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", async () => {
+  const activeTab = getActiveTabURL();
+  const queryParameters = activeTab.url.split("?")[1];
+  const urlParameters = new URLSearchParams(queryParameters);
+  const currentVideo = urlParameters.get("v");
+  if (activeTab.tab.url.includes("youtube.com/watch") && currentVideo) {
+    chrome.storage.sync.get([currentVideo], (data) => {
+      const currentVideoBookmarks = data[currentVideo]
+        ? JSON.parse(data[currentVideo])
+        : [];
+    });
+  }
+});
